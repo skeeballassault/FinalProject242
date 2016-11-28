d016511f5b7befcdbabd75e8a82effa651284006
jacob <jacob@client-104-39-99-183.mobility.psu.edu>
2016-11-28 15:37:09 -0500

    Added (non-functional) option controls to OptionView

--- src/OptionsView.java
+++ src/OptionsView.java
@@ -1,5 +1,7 @@
 
+import java.util.Hashtable;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -11,32 +13,61 @@ import javax.swing.JTextField;
  */
 public class OptionsView extends JPanel{
         JButton retButton;
-        JTextField playerName;
+        
         JLabel EnterDiff;
+        JLabel ChooseGridSize;
+        JLabel ChooseColorAmount;
+        
         JSlider difficulty;
+        JComboBox gridSizeList;
+        JComboBox colorAmountList;
         
         OptionsModel o_model;
         
         OptionsView(OptionsModel o_model)
         {
             this.o_model = o_model;
-                   
             
-            playerName = new JTextField("--Enter Player Name--");
-            difficulty = new JSlider(JSlider.HORIZONTAL, 1,10,5);
+            
+            difficulty = new JSlider(JSlider.HORIZONTAL, 1,3,1);
             difficulty.setMajorTickSpacing(1);
-            difficulty.setPaintLabels(true);
+            
             difficulty.setPaintTicks(true);
+            Hashtable diffLabels = new Hashtable();
+            diffLabels.put(1, new JLabel("Easy"));
+            diffLabels.put(2, new JLabel("Medium"));
+            diffLabels.put(3, new JLabel("Hard"));
+            difficulty.setLabelTable(diffLabels);
+            
+            difficulty.setPaintLabels(true);
+            
+            difficulty.setSnapToTicks(true);
 
-            EnterDiff = new JLabel("  Enter Difficulty:");
 
+            String[] gridSizes = { "2x2", "6x6", "10x10", "14x14", "18x18", "22x22", "26x26" };
+
+            gridSizeList = new JComboBox(gridSizes);
+            gridSizeList.setSelectedIndex(3);
+            
+            Integer[] colorAmounts = { 3,4,5,6,7,8 };
+            
+            colorAmountList = new JComboBox(colorAmounts);
+            colorAmountList.setSelectedIndex(3);
+            
+            EnterDiff = new JLabel("Enter Difficulty:");
+            ChooseGridSize = new JLabel("Choose Grid Size:");
+            ChooseColorAmount = new JLabel("Choose Color Amount:");
+            
             // It may be useful to have a Save & Return button
             
             //retButton = new JButton("Save and Return");
 
-            add(playerName);
             add(EnterDiff);
             add(difficulty);
+            add(ChooseGridSize);
+            add(gridSizeList);
+            add(ChooseColorAmount);
+            add(colorAmountList);
 
             //add(retButton);               
         }
